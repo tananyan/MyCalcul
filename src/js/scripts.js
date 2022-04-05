3f60df030d9df4f37568e387836ad0453cf18189
Boris <Boris242008@ya.ru>
2022-04-05 22:50:48 +0300

    1

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,38 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
   const funs = document.querySelectorAll(".fun");
   const res = document.querySelector(".res");
   const clean = document.querySelector(".top-fun");
-  const dispLay = document.querySelectorAll(".top-num");
+  const dispLay = document.querySelector(".top-num");
   const operator = document.querySelector(".top-operator");
   const plusMinus = document.querySelector(".fun-inv");
 
-  let numberOne = 0,
-    numberTwo = 0,
-    flag = false;
-
   numbers.forEach((num) => {
-    // numberOne
-    if (flag == false) {
-      console.log('if false');
-      num.addEventListener("click", () => {
-        dispLay[0].innerHTML += num.innerHTML;
-      });
-      numberOne = dispLay[0].innerHTML;
-    }
-    // numberTwo
-    if (flag == true) {
-      console.log('if true');
-      num.addEventListener("click", () => {
-        dispLay[1].innerHTML += num.innerHTML;
-      });
-      numberOne = dispLay[1].innerHTML;
-    }
+    console.log("if false");
+    num.addEventListener("click", () => {
+      dispLay.innerHTML += num.innerHTML;
+    });
   });
 
   funs.forEach((fun) => {
     fun.addEventListener("click", () => {
       operator.innerHTML = fun.innerHTML;
-      flag = true;
-      console.log(`flag: ${flag}`);
     });
   });
 
